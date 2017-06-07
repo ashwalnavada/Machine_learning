b4aef410bb27f1183c320a101a04b1af52bdd2e8
ashwalnavada <ashwal.kudremukh@gmail.com>
2017-06-06 20:01:27 -0400

    updating visualization 2 file

--- visualization2.R
+++ visualization2.R
@@ -24,3 +24,15 @@ data(Population)
 head(Population)
 w <-gvisGeoMap(Population,locationvar = 'Country', numvar= "Population")
 plot(w)
+
+#Pivot table/chart
+
+library(devtools)
+install_github("ramnathv/htmlwidgets") 
+install_github("smartinsightsfromdata/rpivotTable")
+## Load rpivotTable
+library(rpivotTable)
+
+## One line to create pivot table
+rpivotTable(vehicle, rows="State", col="Count", aggregatorName="Count", 
+            vals="State", rendererName="Treemap")
